edba549046a2b5d2fdef050872a9a9a4f3ead3b8
Razibul Islam <dev.ronib@gmail.com>
2024-01-20 02:57:14 +0600

    Carousel section added

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,10 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{html,js}"],
   theme: {
-    extend: {},
+    colors: {
+      clifford: "#da373d",
+      "bike-primary": "#e76f51",
+      "bike-primary-bg": "rgba(231, 111, 81, 0.10)",
+    },
   },
-  plugins: [],
 };
